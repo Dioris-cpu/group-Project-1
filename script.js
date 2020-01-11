6f2d1058da3d1afebd37d1356cf59e50d980fae6
My Lord <page.c.tyler@gmail.com>
2020-01-11 10:02:27 -0500

    user location map added

--- script.js
+++ script.js
@@ -9,67 +9,56 @@ $(document).ready(function () {
         geo = navigator.geolocation;
 
 setMapCanvas();
+setUsersCurrentPosition();
 
 
+$('#searchBtn').on("click", function () {
+    var city = places.value
+    var queryURL = "https://developers.zomato.com/api/v2.1/locations?query=" + city + "&apikey=" + apikey;
+    $.ajax({
+        url: queryURL,
+        cors: true
+    }).then(data => {
+        var entityId = data.location_suggestions[0].entity_id;
+        var entityType = data.location_suggestions[0].entity_type;
+        var queryURL = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + entityId + "&entity_type=" + entityType + "&apikey=" + apikey;
+        $.ajax({
+            url: queryURL,
+            cors: true
+        }).then(data => {
+            console.log(data);
+        })
+    })
+})
 
-    // $.ajax({
-    //     url: queryURL,
-    //     cors: true
-    // }).then(function(data) {
-    //     console.log(data) 
-    // })
-    // $('.searchBtn').on("click", function () {
-    //     var city = places.value
-    //     var queryURL = "https://developers.zomato.com/api/v2.1/locations?query=" + city + "&apikey=" + apikey;
-    //     $.ajax({
-    //         url: queryURL,
-    //         cors: true
-    //     }).then(data => {
-    //         var entityId = data.location_suggestions[0].entity_id;
-    //         var entityType = data.location_suggestions[0].entity_type;
-    //         var queryURL = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + entityId + "&entity_type=" + entityType + "&apikey=" + apikey;
-    //         $.ajax({
-    //             url: queryURL,
-    //             cors: true
-    //         }).then(data => {
-    //             console.log(data);
-    //         })
-    //     })
-       
-           
-            
-        setUsersCurrentPosition()
-        function setUsersCurrentPosition() {
-            geo.getCurrentPosition(function (position) {
-                lat = position.coords.latitude;
-                lng = position.coords.longitude;
-                var coords = {lat : lat, lng : lng};
-                console.log(coords);
-                $.ajax({
-                    url: "https://developers.zomato.com/api/v2.1/geocode?lat=" + lat + "&lon=" + lng + "&apikey=" + zamatoKey + "",
-                    method: "GET"
-                }).then(function (food) {
-                    console.log(food);
-                    var wellSection = document.getElementById("wellSection")
-                    var allRest = food.nearby_restaurants;
-                    allRest.forEach(function(data){ 
-                        var info = data.restaurant;
-                        var p = document.createElement("p");
-                        var cuisines = info.cuisines;
-                        var apikey = info.apikey;
-                        p.innerText = cuisines + apikey;
-                        wellSection.appendChild(p)
-                    })
-                    var apikey
-                    
-                })
-                initMap(coords);
+function setUsersCurrentPosition() {
+    geo.getCurrentPosition(function (position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        var coords = {lat : lat, lng : lng};
+        console.log(coords);
+        $.ajax({
+            url: "https://developers.zomato.com/api/v2.1/geocode?lat=" + lat + "&lon=" + lng + "&apikey=" + zamatoKey + "",
+            method: "GET"
+        }).then(function (food) {
+            console.log(food);
+            var wellSection = document.getElementById("wellSection")
+            var allRest = food.nearby_restaurants;
+            allRest.forEach(function(data){ 
+                var info = data.restaurant;
+                var p = document.createElement("p");
+                var cuisines = info.cuisines;
+                var apikey = info.apikey;
+                p.innerText = cuisines + apikey;
+                wellSection.appendChild(p);
             })
-        
-                $('.class').text('variable');
-        $("wellSection").empty();
-        }    
-    
+        })
+        initMap(coords);
+    })
+        $('.class').text('variable');
+$("wellSection").empty();
+}    
+
 
 function setMapCanvas() {
     var script = document.createElement("script");
